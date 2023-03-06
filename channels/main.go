61ac0c8178c81781f3a15b900f8d8f4e7f3ffe1a
David Regan <83370854+davidrobertregan@users.noreply.github.com>
2023-03-06 13:49:58 -0700

    finish channels

--- channels/main.go
+++ channels/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"time"
 )
 
 func main() {
@@ -14,18 +15,34 @@ func main() {
 		"http://amazon.com",
 	}
 
+	c := make(chan string)
+
 	for _, l := range links {
-		go checkStatus(l)
+		go checkLink(l, c)
 	}
+
+	for l := range c { /* same as below, but better syntax for future devs */
+		go func(link string) { /* function literal, the same as JS anonymous function */
+			time.Sleep(5 * time.Second)
+			checkLink(link, c)
+		}(l)
+	}
+
+	// for  {  /* infinite loop */
+	// 	go checkLink(<-c, c)
+	// }
+
 }
 
-func checkStatus(link string) {
+func checkLink(link string, c chan string) {
 	_, err := http.Get(link)
 
 	if err != nil {
 		fmt.Println(link, "might be down")
+		c <- link
 		return
 	}
 
 	fmt.Println(link, "is up")
+	c <- link
 }
